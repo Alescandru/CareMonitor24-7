86f04c92449a6d1074be101f53acb6ad5ac96fb5
Parsyfall <dumitru.tronciu@gmail.com>
2024-10-24 12:25:02 +0300

    feat: make AppDatabase a singleton - Not a thread safe one but will do for now

--- app/src/main/java/com/example/hackathon2024/database/AppDatabase.java
+++ app/src/main/java/com/example/hackathon2024/database/AppDatabase.java
@@ -1,9 +1,22 @@
 package com.example.hackathon2024.database;
 
-import androidx.room.Database;
+import android.content.Context;
 
-// TODO: Make it singleton
-@Database(entities = {HealthRecord.class}, version = 1)
-public abstract class AppDatabase {
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {HealthRecord.class}, version = 1, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
     public abstract HealthRecordDao healthRecordDao();
+
+    private static AppDatabase instance = null;
+
+    public static AppDatabase getInstance(Context context) {
+        if (null == instance) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                    AppDatabase.class, "care_monitor").build();
+        }
+        return instance;
+    }
 }
