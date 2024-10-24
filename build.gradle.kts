d31cdb5fa2de3344a792355a8093958a6435a78f
Alescandru <davidalexandurgabriel31@gmail.com>
2024-10-24 23:12:07 +0300

    sunt erori la salvare/afisare nume. si la analiza datelor. Merge generare de graficuri si am mutata creare pdf in raport din main

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,4 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
 }
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://chaquo.com/maven") }
-    }
-}
-
