a23cc9b693855aa49eaf87b441cad3d7803a43f8
Parsyfall <dumitru.tronciu@gmail.com>
2024-10-24 19:39:38 +0300

    feat, refactor: change constructor to a factory method feat: move listAverage to a separate class

--- app/src/main/java/com/example/hackathon2024/database/HealthRecord.java
+++ app/src/main/java/com/example/hackathon2024/database/HealthRecord.java
@@ -1,32 +1,77 @@
 package com.example.hackathon2024.database;
 
+import static com.example.hackathon2024.utils.Utils.listAverage;
+
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import java.util.Collections;
+import java.util.List;
+
 // TODO: Change field visibility to private, add getters and setters
 
-@Entity(tableName = "health_records")
+/**
+ * Vital metrics record for a period of time
+ */
+@Entity(tableName = "daily_raw_data")
 public class HealthRecord {
     @PrimaryKey(autoGenerate = true)
     public int id;
+    @ColumnInfo(name = "hear_beat_min")
+    public int heartBeatMin;
 
-    @ColumnInfo(name = "hear_beat")
-    public int hearBeat;
+    @ColumnInfo(name = "hear_beat_max")
+    public int hearBeatMax;
 
-    @ColumnInfo(name = "oxygen_level")
-    public int oxygenLevel;
+    @ColumnInfo(name = "hear_beat_avg")
+    public double hearBeatAvg;
 
-    @ColumnInfo(name = "systolic_pressure")
-    public int systolicPressure;
+    @ColumnInfo(name = "oxygen_level_min")
+    public int oxygenLevelMin;
 
-    @ColumnInfo(name = "diastolic_pressure")
-    public int diastolicPressure;
+    @ColumnInfo(name = "oxygen_level_max")
+    public int oxygenLevelMax;
 
-    public HealthRecord(int hearBeat, int oxygenLevel, int systolicPressure, int diastolicPressure){
-        this.hearBeat = hearBeat;
-        this.oxygenLevel = oxygenLevel;
-        this.systolicPressure = systolicPressure;
-        this.diastolicPressure = diastolicPressure;
+    @ColumnInfo(name = "oxygen_level_avg")
+    public double oxygenLevelAvg;
+
+    @ColumnInfo(name = "systolic_pressure_max")
+    public int systolicPressureMax;
+
+    @ColumnInfo(name = "systolic_pressure_min")
+    public int systolicPressureMin;
+
+    @ColumnInfo(name = "systolic_pressure_avg")
+    public double systolicPressureAvg;
+
+    @ColumnInfo(name = "diastolic_pressure_max")
+    public int diastolicPressureMax;
+
+    @ColumnInfo(name = "diastolic_pressure_min")
+    public int diastolicPressureMin;
+
+    @ColumnInfo(name = "diastolic_pressure_avg")
+    public double diastolicPressureAvg;
+
+    public static HealthRecord create(List<Integer> hearBeatList, List<Integer> oxygenLevelList, List<Integer> systolicPressureList, List<Integer> diastolicPressureList){
+        HealthRecord record = new HealthRecord();
+        record.heartBeatMin = Collections.min(hearBeatList);
+        record.hearBeatMax = Collections.max(hearBeatList);
+        record.hearBeatAvg = listAverage(hearBeatList);
+
+        record.oxygenLevelMin = Collections.min(oxygenLevelList);
+        record.oxygenLevelMax = Collections.max(oxygenLevelList);
+        record.oxygenLevelAvg = listAverage(oxygenLevelList);
+
+        record.systolicPressureMin = Collections.min(systolicPressureList);
+        record.systolicPressureMax = Collections.max(systolicPressureList);
+        record.systolicPressureAvg = listAverage(systolicPressureList);
+
+        record.diastolicPressureMin = Collections.min(diastolicPressureList);
+        record.diastolicPressureMax = Collections.max(diastolicPressureList);
+        record.diastolicPressureAvg = listAverage(diastolicPressureList);
+
+        return record;
     }
 }
